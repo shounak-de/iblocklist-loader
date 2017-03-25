d23985bb7e50c07088e95472ebca767503ffd9d9
redhat27 <shounak.de@gmail.com>
2017-03-25 10:00:10 -0700

    Added handling whitelist domains

--- iblocklist-loader.sh
+++ iblocklist-loader.sh
@@ -5,57 +5,59 @@
 # snbforums thread: https://www.snbforums.com/threads/iblocklist-com-generic-ipset-loader-for-ipset-v6-and-v4.37976/
 # credits for v6 implementation: http://www.unix.com/shell-programming-and-scripting/233825-convert-ip-ranges-cidr-netblocks.html
 
-# Available free block lists from [https://www.iblocklist.com/lists] Format:
-# Index------Blocklist name------Maintainer--Download URL-------------------------------------------------------------------------
-BlockList01="Pedophiles          I-Blocklist http://list.iblocklist.com/?list=dufcxgnbjsdwmwctgfuj&fileformat=p2p&archiveformat=gz"
-BlockList02="level1              Bluetack    http://list.iblocklist.com/?list=ydxerpxkpcfqjaybcssw&fileformat=p2p&archiveformat=gz"
-BlockList03="level2              Bluetack    http://list.iblocklist.com/?list=gyisgnzbhppbvsphucsw&fileformat=p2p&archiveformat=gz"
-BlockList04="level3              Bluetack    http://list.iblocklist.com/?list=uwnukjqktoggdknzrhgh&fileformat=p2p&archiveformat=gz"
-BlockList05="edu                 Bluetack    http://list.iblocklist.com/?list=imlmncgrkbnacgcwfjvh&fileformat=p2p&archiveformat=gz"
-BlockList06="rangetest           Bluetack    http://list.iblocklist.com/?list=plkehquoahljmyxjixpu&fileformat=p2p&archiveformat=gz"
-BlockList07="bogon               Bluetack    http://list.iblocklist.com/?list=gihxqmhyunbxhbmgqrla&fileformat=p2p&archiveformat=gz"
-BlockList08="ads                 Bluetack    http://list.iblocklist.com/?list=dgxtneitpuvgqqcpfulq&fileformat=p2p&archiveformat=gz"
-BlockList09="spyware             Bluetack    http://list.iblocklist.com/?list=llvtlsjyoyiczbkjsxpf&fileformat=p2p&archiveformat=gz"
-BlockList10="proxy               Bluetack    http://list.iblocklist.com/?list=xoebmbyexwuiogmbyprb&fileformat=p2p&archiveformat=gz"
-BlockList11="badpeers            Bluetack    http://list.iblocklist.com/?list=cwworuawihqvocglcoss&fileformat=p2p&archiveformat=gz"
-BlockList12="Microsoft           Bluetack    http://list.iblocklist.com/?list=xshktygkujudfnjfioro&fileformat=p2p&archiveformat=gz"
-BlockList13="spider              Bluetack    http://list.iblocklist.com/?list=mcvxsnihddgutbjfbghy&fileformat=p2p&archiveformat=gz"
-BlockList14="hijacked            Bluetack    http://list.iblocklist.com/?list=usrcshglbiilevmyfhse&fileformat=p2p&archiveformat=gz"
-BlockList15="dshield             Bluetack    http://list.iblocklist.com/?list=xpbqleszmajjesnzddhv&fileformat=p2p&archiveformat=gz"
-BlockList16="forumspam           Bluetack    http://list.iblocklist.com/?list=ficutxiwawokxlcyoeye&fileformat=p2p&archiveformat=gz"
-BlockList17="webexploit          Bluetack    http://list.iblocklist.com/?list=ghlzqtqxnzctvvajwwag&fileformat=p2p&archiveformat=gz"
-BlockList18="iana-reserved       Bluetack    http://list.iblocklist.com/?list=bcoepfyewziejvcqyhqo&fileformat=p2p&archiveformat=gz"
-BlockList19="iana-private        Bluetack    http://list.iblocklist.com/?list=cslpybexmxyuacbyuvib&fileformat=p2p&archiveformat=gz"
-BlockList20="iana-multicast      Bluetack    http://list.iblocklist.com/?list=pwqnlynprfgtjbgqoizj&fileformat=p2p&archiveformat=gz"
-BlockList21="fornonlancomputers  Bluetack    http://list.iblocklist.com/?list=jhaoawihmfxgnvmaqffp&fileformat=p2p&archiveformat=gz"
-BlockList22="exclusions          Bluetack    http://list.iblocklist.com/?list=mtxmiireqmjzazcsoiem&fileformat=p2p&archiveformat=gz"
-BlockList23="DROP                Spamhaus    http://list.iblocklist.com/?list=zbdlwrqkabxbcppvrnos&fileformat=p2p&archiveformat=gz"
-BlockList24="ZeuS                abuse       http://list.iblocklist.com/?list=ynkdjqsjyfmilsgbogqf&fileformat=p2p&archiveformat=gz"
-BlockList25="SpyEye              abuse       http://list.iblocklist.com/?list=zvjxsfuvdhoxktpeiokq&fileformat=p2p&archiveformat=gz"
-BlockList26="Palevo              abuse       http://list.iblocklist.com/?list=erqajhwrxiuvjxqrrwfj&fileformat=p2p&archiveformat=gz"
-BlockList27="Malicious           CI-Army     http://list.iblocklist.com/?list=npkuuhuxcsllnhoamkvm&fileformat=p2p&archiveformat=gz"
-BlockList28="malc0de             malc0de     http://list.iblocklist.com/?list=pbqcylkejciyhmwttify&fileformat=p2p&archiveformat=gz"
-BlockList29="adservers           Yoyo        http://list.iblocklist.com/?list=zhogegszwduurnvsyhdf&fileformat=p2p&archiveformat=gz"
-BlockList30="bogon               cidr-report http://list.iblocklist.com/?list=lujdnbasfaaixitgmxpp&fileformat=p2p&archiveformat=gz"
-BlockList31="cruzit-web-attacks  CruzIT      http://list.iblocklist.com/?list=czvaehmjpsnwwttrdoyl&fileformat=p2p&archiveformat=gz"
-BlockList32="Business-ISPs       TBG         http://list.iblocklist.com/?list=jcjfaxgyyshvdbceroxf&fileformat=p2p&archiveformat=gz"
-BlockList33="Primary-Threats     TBG         http://list.iblocklist.com/?list=ijfqtofzixtwayqovmxn&fileformat=p2p&archiveformat=gz"
-BlockList34="Hijacked            TBG         http://list.iblocklist.com/?list=tbnuqfclfkemqivekikv&fileformat=p2p&archiveformat=gz"
-BlockList35="Bogon               TBG         http://list.iblocklist.com/?list=ewqglwibdgjttwttrinl&fileformat=p2p&archiveformat=gz"
-BlockList36="Search-Engines      TBG         http://list.iblocklist.com/?list=pfefqteoxlfzopecdtyw&fileformat=p2p&archiveformat=gz"
-BlockList37="Corporate-Ranges    TBG         http://list.iblocklist.com/?list=ecqbsykllnadihkdirsh&fileformat=p2p&archiveformat=gz"
+# Available free lists from [https://www.iblocklist.com/lists] Format:
+# ------List name-----------Maintainer--Download URL-------------------------------------------------------------------------
+List01="Pedophiles          I-Blocklist http://list.iblocklist.com/?list=dufcxgnbjsdwmwctgfuj&fileformat=p2p&archiveformat=gz"
+List02="level1              Bluetack    http://list.iblocklist.com/?list=ydxerpxkpcfqjaybcssw&fileformat=p2p&archiveformat=gz"
+List03="level2              Bluetack    http://list.iblocklist.com/?list=gyisgnzbhppbvsphucsw&fileformat=p2p&archiveformat=gz"
+List04="level3              Bluetack    http://list.iblocklist.com/?list=uwnukjqktoggdknzrhgh&fileformat=p2p&archiveformat=gz"
+List05="edu                 Bluetack    http://list.iblocklist.com/?list=imlmncgrkbnacgcwfjvh&fileformat=p2p&archiveformat=gz"
+List06="rangetest           Bluetack    http://list.iblocklist.com/?list=plkehquoahljmyxjixpu&fileformat=p2p&archiveformat=gz"
+List07="bogon               Bluetack    http://list.iblocklist.com/?list=gihxqmhyunbxhbmgqrla&fileformat=p2p&archiveformat=gz"
+List08="ads                 Bluetack    http://list.iblocklist.com/?list=dgxtneitpuvgqqcpfulq&fileformat=p2p&archiveformat=gz"
+List09="spyware             Bluetack    http://list.iblocklist.com/?list=llvtlsjyoyiczbkjsxpf&fileformat=p2p&archiveformat=gz"
+List10="proxy               Bluetack    http://list.iblocklist.com/?list=xoebmbyexwuiogmbyprb&fileformat=p2p&archiveformat=gz"
+List11="badpeers            Bluetack    http://list.iblocklist.com/?list=cwworuawihqvocglcoss&fileformat=p2p&archiveformat=gz"
+List12="Microsoft           Bluetack    http://list.iblocklist.com/?list=xshktygkujudfnjfioro&fileformat=p2p&archiveformat=gz"
+List13="spider              Bluetack    http://list.iblocklist.com/?list=mcvxsnihddgutbjfbghy&fileformat=p2p&archiveformat=gz"
+List14="hijacked            Bluetack    http://list.iblocklist.com/?list=usrcshglbiilevmyfhse&fileformat=p2p&archiveformat=gz"
+List15="dshield             Bluetack    http://list.iblocklist.com/?list=xpbqleszmajjesnzddhv&fileformat=p2p&archiveformat=gz"
+List16="forumspam           Bluetack    http://list.iblocklist.com/?list=ficutxiwawokxlcyoeye&fileformat=p2p&archiveformat=gz"
+List17="webexploit          Bluetack    http://list.iblocklist.com/?list=ghlzqtqxnzctvvajwwag&fileformat=p2p&archiveformat=gz"
+List18="iana-reserved       Bluetack    http://list.iblocklist.com/?list=bcoepfyewziejvcqyhqo&fileformat=p2p&archiveformat=gz"
+List19="iana-private        Bluetack    http://list.iblocklist.com/?list=cslpybexmxyuacbyuvib&fileformat=p2p&archiveformat=gz"
+List20="iana-multicast      Bluetack    http://list.iblocklist.com/?list=pwqnlynprfgtjbgqoizj&fileformat=p2p&archiveformat=gz"
+List21="fornonlancomputers  Bluetack    http://list.iblocklist.com/?list=jhaoawihmfxgnvmaqffp&fileformat=p2p&archiveformat=gz"
+List22="exclusions          Bluetack    http://list.iblocklist.com/?list=mtxmiireqmjzazcsoiem&fileformat=p2p&archiveformat=gz"
+List23="DROP                Spamhaus    http://list.iblocklist.com/?list=zbdlwrqkabxbcppvrnos&fileformat=p2p&archiveformat=gz"
+List24="ZeuS                abuse       http://list.iblocklist.com/?list=ynkdjqsjyfmilsgbogqf&fileformat=p2p&archiveformat=gz"
+List25="SpyEye              abuse       http://list.iblocklist.com/?list=zvjxsfuvdhoxktpeiokq&fileformat=p2p&archiveformat=gz"
+List26="Palevo              abuse       http://list.iblocklist.com/?list=erqajhwrxiuvjxqrrwfj&fileformat=p2p&archiveformat=gz"
+List27="Malicious           CI-Army     http://list.iblocklist.com/?list=npkuuhuxcsllnhoamkvm&fileformat=p2p&archiveformat=gz"
+List28="malc0de             malc0de     http://list.iblocklist.com/?list=pbqcylkejciyhmwttify&fileformat=p2p&archiveformat=gz"
+List29="adservers           Yoyo        http://list.iblocklist.com/?list=zhogegszwduurnvsyhdf&fileformat=p2p&archiveformat=gz"
+List30="bogon               cidr-report http://list.iblocklist.com/?list=lujdnbasfaaixitgmxpp&fileformat=p2p&archiveformat=gz"
+List31="cruzit-web-attacks  CruzIT      http://list.iblocklist.com/?list=czvaehmjpsnwwttrdoyl&fileformat=p2p&archiveformat=gz"
+List32="Business-ISPs       TBG         http://list.iblocklist.com/?list=jcjfaxgyyshvdbceroxf&fileformat=p2p&archiveformat=gz"
+List33="Primary-Threats     TBG         http://list.iblocklist.com/?list=ijfqtofzixtwayqovmxn&fileformat=p2p&archiveformat=gz"
+List34="Hijacked            TBG         http://list.iblocklist.com/?list=tbnuqfclfkemqivekikv&fileformat=p2p&archiveformat=gz"
+List35="Bogon               TBG         http://list.iblocklist.com/?list=ewqglwibdgjttwttrinl&fileformat=p2p&archiveformat=gz"
+List36="Search-Engines      TBG         http://list.iblocklist.com/?list=pfefqteoxlfzopecdtyw&fileformat=p2p&archiveformat=gz"
+List37="Corporate-Ranges    TBG         http://list.iblocklist.com/?list=ecqbsykllnadihkdirsh&fileformat=p2p&archiveformat=gz"
 
-BLOCKLIST_INDEXES="12 33 9" # Can be any combination of above list indexes, e.g "27 9", "1", "7 24 8 29 31" etc. [Example: PeerGuardian implementation would be "2 11"]
+BLOCKLIST_INDEXES="33 9" # Can be any combination of above list indexes, e.g "15 13", "1", "7 24 8 29 31" etc. [Example: PeerGuardian implementation would be "2 11"]
+WHITELIST_DOMAINS_FILE="/jffs/ipset_lists/whitelist-domains.txt" # One line per domain, comments (starting with the '#' character) allowed, even inline comments
 
 # Use locally cached ipset data or download on each run
 USE_LOCAL_CACHE=Y # [Y|N]
 
 # Re-download blocklist data if locally saved files are older than this many days [Needed mostly for USE_LOCAL_CACHE=Y]
-BLOCKLISTS_SAVE_DAYS=10
+LISTS_SAVE_DAYS=10
 
 # Use DROP or REJECT target for iptable rule. Briefly, for DROP, attacker (or IP being blocked) will get no response and timeout,
 # and REJECT will send immediate response of destination-unreachable (Attacker will know your IP is actively rejecting requests)
 # See: http://www.chiark.greenend.org.uk/~peterb/network/drop-vs-reject and http://serverfault.com/questions/157375/reject-vs-drop-when-using-iptables
+# or from our own RMerlin: https://www.snbforums.com/threads/ip-tables-confusion.30373/#post-237738
 IPTABLES_RULE_TARGET=DROP # [DROP|REJECT]
 
 # Folder to cache downloaded files [Needed for USE_LOCAL_CACHE=Y or storing the file for posterity]
@@ -82,12 +84,13 @@ case $(ipset -v | grep -o "v[4,6]") in
     for module in ip_set ip_set_nethash ip_set_iphash xt_set; do
       insmod $module
     done;
+    MATCH_SET='--match-set'; CREATE='create'; DESTROY='destroy'; ADD='add'; IPHASH='hash:ip'
     ipset destroy tIP 2>/dev/null; ipset destroy tNet 2>/dev/null # Recover if previous run aborted
     for index in $BLOCKLIST_INDEXES; do
       [ ${#index} -eq 1 ] && index="0${index}"
-      SetName=$(eval echo \$$(eval echo BlockList${index}) | awk '{ print toupper(substr($2,1,1)) substr($2,2) toupper(substr($1,1,1)) substr($1,2) }')
-      Url=$(eval echo \$$(eval echo BlockList${index}) | awk '{ print $3 }')
-      [ ! -s "$IPSET_LISTS_DIR/${SetName}.gz" -o -n "$(find $IPSET_LISTS_DIR/${SetName}.gz -mtime +$BLOCKLISTS_SAVE_DAYS -print 2>/dev/null)" ] && wget -q -O $IPSET_LISTS_DIR/${SetName}.gz ${Url}
+      SetName=$(eval echo \$$(eval echo List${index}) | awk '{ print toupper(substr($2,1,1)) substr($2,2) toupper(substr($1,1,1)) substr($1,2) }')
+      Url=$(eval echo \$$(eval echo List${index}) | awk '{ print $3 }')
+      [ ! -s "$IPSET_LISTS_DIR/${SetName}.gz" -o -n "$(find $IPSET_LISTS_DIR/${SetName}.gz -mtime +$LISTS_SAVE_DAYS -print 2>/dev/null)" ] && wget -q -O $IPSET_LISTS_DIR/${SetName}.gz ${Url}
       [ "$USE_LOCAL_CACHE" = "Y" ] && GetCommand="cat $IPSET_LISTS_DIR/${SetName}.gz" || GetCommand="wget -q -O - ${Url}"
 
       # Create the sets if they do not exist
@@ -146,12 +149,13 @@ case $(ipset -v | grep -o "v[4,6]") in
     for module in ip_set ip_set_iptreemap ipt_set; do
       insmod $module
     done;
+    MATCH_SET='--set'; CREATE='--create'; DESTROY='--destroy'; ADD='--add'; IPHASH='iphash'
     ipset --destroy iBTmp 2>/dev/null # Recover if previous run aborted
     for index in $BLOCKLIST_INDEXES; do
       [ ${#index} -eq 1 ] && index="0${index}"
-      SetName=$(eval echo \$$(eval echo BlockList${index}) | awk '{ print toupper(substr($2,1,1)) substr($2,2) toupper(substr($1,1,1)) substr($1,2) }')
-      Url=$(eval echo \$$(eval echo BlockList${index}) | awk '{ print $3 }')
-      [ ! -s "$IPSET_LISTS_DIR/${SetName}.gz" -o -n "$(find $IPSET_LISTS_DIR/${SetName}.gz -mtime +$BLOCKLISTS_SAVE_DAYS -print 2>/dev/null)" ] && wget -q -O $IPSET_LISTS_DIR/${SetName}.gz ${Url}
+      SetName=$(eval echo \$$(eval echo List${index}) | awk '{ print toupper(substr($2,1,1)) substr($2,2) toupper(substr($1,1,1)) substr($1,2) }')
+      Url=$(eval echo \$$(eval echo List${index}) | awk '{ print $3 }')
+      [ ! -s "$IPSET_LISTS_DIR/${SetName}.gz" -o -n "$(find $IPSET_LISTS_DIR/${SetName}.gz -mtime +$LISTS_SAVE_DAYS -print 2>/dev/null)" ] && wget -q -O $IPSET_LISTS_DIR/${SetName}.gz ${Url}
       [ "$USE_LOCAL_CACHE" = "Y" ] && GetCommand="cat $IPSET_LISTS_DIR/${SetName}.gz" || GetCommand="wget -q -O - ${Url}"
 
       # Create the set if it does not exist
@@ -169,6 +173,23 @@ case $(ipset -v | grep -o "v[4,6]") in
       logger -t Firewall "$0: Loaded ${SetName} blocklist with $(ipset -L ${SetName} | wc -l | awk '{print $1-6}') entries"
     done;;
   *)
-    logger -t Firewall "$0: Unknown ipset version: $(ipset -v). Exiting."
+    logger -t Firewall "$0: Unknown ipset version. Exiting."
     exit 1;;
 esac
+if [ -s "$WHITELIST_DOMAINS_FILE" ]; then
+  [ ! $(which hostip) ] && echo "hostip utility is needed to process whilelist domains file!" && logger -t Firewall "$0: hostip utility is needed to process whilelist domains file!" && exit 1
+  iptables-save | grep -q WhiteList && iptables -D FORWARD -m set $MATCH_SET WhiteList src,dst -j ACCEPT
+  ipset $DESTROY WhiteList &>/dev/null # Destroy *if* existing (It will exist if this script is run more than once, e.g. scheduled in cron)
+  ipset $CREATE WhiteList $IPHASH
+  [ $? -eq 0 ] && entryCount=0
+  while read line; do
+    if [ -n "${line%%#*}" ]; then
+      for ip in $(hostip ${line%%#*}); do
+        ipset $ADD WhiteList $ip
+        [ $? -eq 0 ] && entryCount=$((entryCount+1))
+      done
+    fi
+  done <$WHITELIST_DOMAINS_FILE
+  logger -t Firewall "$0: Added WhiteList ($entryCount entries)"
+  iptables-save | grep -q WhiteList || iptables -I FORWARD -m set $MATCH_SET WhiteList src,dst -j ACCEPT
+fi
