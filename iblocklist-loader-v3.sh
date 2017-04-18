c80c15f057a71c1c99bcbcf68ab6e15d226a7a27
redhat27 <shounak.de@gmail.com>
2017-04-18 14:51:21 -0700

    Update iblocklist-loader-v3.sh

--- iblocklist-loader-v3.sh
+++ iblocklist-loader-v3.sh
@@ -1,2 +1,2 @@
-# This script has been merged to https://github.com/shounak-de/iblocklist-loader/edit/master/iblocklist-loader-v2.sh
+# This script has been merged to https://github.com/shounak-de/iblocklist-loader/blob/master/iblocklist-loader-v2.sh
 # Please use that script instead
